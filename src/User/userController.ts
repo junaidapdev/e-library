4dd6fd6d66a226de223aabdaf5991d438d2703fd
junaidapdev <junaidap.dev@gmail.com>
2024-05-14 14:10:57 +0530

    add paassword hash

--- src/User/userController.ts
+++ src/User/userController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Response, Request } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
+import bcrypt from "bcrypt";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
@@ -18,6 +19,22 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
     if (user) {
         const error = createHttpError(400, 'User already exists with the same email');
+
+        return next(error);
+    }
+
+    // Password - Hashing
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Database call
+    const newUser = await userModel.create({
+        name,
+        email,
+        password:  hashedPassword
+    })
+
+    if (!newUser) {
+        const error = createHttpError(500, 'Something went wrong');
 
         return next(error);
     }
